73b1f57b580305635677b2dc45393f37d9f9f5b6
Henry Van Styn <vanstyn@intellitree.com>
2012-12-26 07:25:18 -0500

    Initial commit

---
+++ lib/DBIx/Class/AuditAny/DataPoint.pm
@@ -0,0 +1,33 @@
+package DBIx::Class::AuditAny::DataPoint;
+use Moose;
+
+# VERSION
+
+use Switch qw(switch);
+
+has 'AuditObj', is => 'ro', isa => 'DBIx::Class::AuditAny', required => 1;
+has 'name', is => 'ro', isa => 'Str', required => 1;
+has 'context', is => 'ro', isa => 'Str', required => 1;
+has 'method', is => 'ro', isa => 'Str|CodeRef', required => 1;
+has 'user_defined', is => 'ro', isa => 'Bool', default => 0;
+
+
+sub BUILD {
+	my $self = shift;
+	
+	my @contexts = qw(base source set change column);
+	die "Bad data point context '" . $self->context . "' - allowed values: " . join(',',@contexts)
+		unless ($self->context ~~ @contexts);
+		
+	die "Bad datapoint name '" . $self->name . "' - only lowercase letters, numbers, underscore(_) and dash(-) allowed" 
+		unless ($self->name =~ /^[a-z0-9\_\-]+$/);
+}
+
+sub get_value {
+	my $self = shift;
+	my $Context = shift;
+	my $method = $self->method;
+	return ref($method) ? $method->($self,$Context,@_) : $Context->$method(@_);
+}
+
+1;
